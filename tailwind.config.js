e712ad43650567a852a113f2d8c7be9f044d2bcd
dxt <prof.mohdali@outlook.com>
2025-02-21 00:53:52 +0530

    unfinished

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+import tailwindcss from '@tailwindcss/vite'
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    tailwindcss(),
+  ],
+}
+
